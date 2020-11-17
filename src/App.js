a42976b99b169c95daeb6958d276763c7c30e74f
Evan Hsu <evanlhsu@gmail.com>
2020-11-18 00:38:42 +0800

    Fix console.log() bug

--- src/App.js
+++ src/App.js
@@ -12,7 +12,6 @@ class App extends React.Component {
   async componentDidMount() {
     const fetchedData = await fetchData();
     this.setState({ data: fetchedData })
-    console.log(data);
   }
 
   render() {
